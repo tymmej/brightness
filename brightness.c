36d24d3c8eea35151d8685fdee28c81f41c6b162
Jakub Tymejczyk <jakub@tymejczyk.pl>
2013-11-17 09:12:18 +0100

    Add licence

--- brightness.c
+++ brightness.c
@@ -1,3 +1,22 @@
+/* brightness.c -- Set brightness based on hour
+This file is part of brightness.
+
+Brightness is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Brightness is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Brightness. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) 2013 Jakub Tymejczyk <tymmej@gmail.com>
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,7 +26,7 @@
 #define TRANSITION_HIGH    3.0
 #define DEG(x)  ((x)*(180/M_PI))
 
-//also from redshift
+/*also from Redshift*/
 float calculate_interpolated_value(double elevation, float day, float night)
 {
 	float result;
